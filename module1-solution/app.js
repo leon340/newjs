572d2c3797fd0efc49651b7c5d0db07e7a830039
LeonBailey <leon.bailey@ucsf.edu>
2017-01-22 20:59:58 -0800

    assignment 1 first submit

---
+++ module1-solution/app.js
@@ -0,0 +1,38 @@
+(function(){
+    'use strict';
+  
+    angular.module('LunchCheck',[])
+    
+    .controller('LunchCheckController', LunchCheckController);
+    
+    LunchCheckController.$inject = ['$scope'];
+    
+    function LunchCheckController($scope){
+        
+        //$scope.errorStyle={};
+      
+       $scope.checkLunch = function (){
+           if(!$scope.lunch || $scope.lunch == " "){
+               $scope.message = "Please enter data first";
+               $scope.errorStyle={"color":"red"};
+           } 
+           
+           else{
+               var numItems = countAmountEaten($scope.lunch);
+               $scope.errorStyle={"color":"green"};
+               if(numItems<=3){
+                   $scope.message = "Enjoy!";
+               }
+               else if(numItems>=3){
+                   $scope.message = "Too Much!";
+               }
+           }
+       }; 
+       
+       function countAmountEaten(string){
+           var items = string.split(',');
+           return items.length;
+       }
+       
+    }
+})();
